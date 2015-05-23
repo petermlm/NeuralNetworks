ab6091a4f10495a80f70c4104ce5bb8030d77afc
petermlm <pedromelgueira@gmail.com>
2015-05-23 14:12:12 +0100

    Put neuron classes in their own file

---
+++ neurons.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+class Neuron:
+    def __init__(self, weights, bias):
+        self.weights = weights
+        self.bias = bias
+
+    def feed(self, input_vals):
+        val = np.dot(self.weights, input_vals) + self.bias
+        return 1 if val > 0 else 0
+
+
+class Perceptron(Neuron):
+    pass
+
+
+class Sigmoid(Neuron):
+    def feed(self, input_vals):
+        val = super().feed(input_vals)
+        return 1 / (1 + np.e ** -val)
+
+
+if __name__ == "__main__":
+    # Make a perception and a sigmoid
+    # The weights are -2 for both inputs, the bias is 3
+    per = Perceptron(np.array([-2, -2]), 3)
+    sig = Sigmoid(np.array([-2, -2]), 3)
+
+    print(per.feed(np.array([0, 0])))
+    print(per.feed(np.array([0, 1])))
+    print(per.feed(np.array([1, 0])))
+    print(per.feed(np.array([1, 1])))
+
+    print(sig.feed(np.array([0, 0])))
+    print(sig.feed(np.array([0, 1])))
+    print(sig.feed(np.array([1, 0])))
+    print(sig.feed(np.array([1, 1])))
