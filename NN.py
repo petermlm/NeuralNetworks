6f68983ecdebaba742fd23212e86c11f5e9ec7d9
petermlm <pedromelgueira@gmail.com>
2015-06-01 17:39:49 +0100

    Fixed problems with Neural Network implementation

--- NN.py
+++ NN.py
@@ -38,18 +38,11 @@ class NeuralNetwork:
         self.activ[0] = np.array(input_vals[:])
 
         # For the following layers, use the normal feed function
-        for k, layer in enumerate(self.layers[1:]):
-            l = k+1
+        for l in range(1, len((self.layers))):
+            z_vec = np.dot(self.weights[l], self.activ[l-1]) + self.bias[l]
+            self.activ[l] = self.activate(z_vec)
 
-            for j in range(layer):
-                w_vec = np.array(self.weights[l][j])
-                x_vec = np.array(self.activ[l-1])
-
-                # Dot product between the values of the previous layer with the
-                # weights, plus the bias
-                z_val = np.dot(w_vec, x_vec) + self.bias[l][j]
-                sig_val = self.activate(z_val)
-                self.activ[l][j] = sig_val
+        return self.activ[-1]
 
     def backPropagation(self, input_vals, output_vals):
         """
@@ -60,14 +53,14 @@ class NeuralNetwork:
         self.feedForward(input_vals)
 
         # Calculate the derivatives using the four equations
-        self.step1(output_vals)
-        self.step2()
-        self.step3()
-        self.step4()
+        self.step1(output_vals)  # Output error
+        self.step2()  # Back propagate error
+        self.step3()  # Calculate the gradient of cost with respect to bias
+        self.step4()  # Calculate the gradient of cost with respect to weights
 
     def step1(self, target):
         """
-        Error for last layer
+        Output error, or error for last layer
         """
 
         L = len(self.layers) - 1
@@ -76,14 +69,14 @@ class NeuralNetwork:
         deriv_vec = self.activ[L] - target
 
         # Calculate sigma vector
-        sig_vec = self.activate(np.dot(self.weights[L], self.activ[L-1]) +
-                                self.bias[L])
+        z_vec = np.dot(self.weights[L], self.activ[L-1]) + self.bias[L]
+        sig_vec = self.activate(z_vec)
 
         self.errors[L] = deriv_vec * sig_vec
 
     def step2(self):
         """
-        Error for layers L-1 to 2
+        Error for layers L-1 to 2, back propagation
         """
 
         L = len(self.layers) - 1
@@ -93,36 +86,30 @@ class NeuralNetwork:
             error_back = np.dot(self.weights[l+1].T, self.errors[l+1])
 
             # Calculate sigma vector
-            sig_vec = self.activate(np.dot(self.weights[l], self.activ[l-1]) +
-                                    self.bias[l])
+            z_vec = np.dot(self.weights[l], self.activ[l-1]) + self.bias[l]
+            sig_vec = self.activate(z_vec)
 
             self.errors[l] = error_back * sig_vec
 
     def step3(self):
         """
-        Derivative of cost function in order of the biases
+        Gradient of cost function in order of the biases
         """
 
         self.cost_b = self.errors
 
     def step4(self):
         """
-        Derivative of cost function in order of the weights
+        Gradient of cost function in order of the weights
         """
 
         self.cost_w = [[]]
 
         for l in range(1, len(self.layers)):
-            rows = self.layers[l]
-            cols = self.layers[l-1]
-            self.cost_w.append(np.zeros((rows, cols)))
+            deriv = np.dot(self.errors[l], self.activ[l-1].T)
+            self.cost_w.append(deriv)
 
-            for j in range(rows):
-                for k in range(cols):
-                    self.cost_w[l][j][k] = \
-                        self.activ[l-1][k] * self.errors[l][j]
-
-    def train(self, training_set, its=300, step=0.5):
+    def train(self, training_set, its=300, step=0.5, verbose=False):
         """
         Given a training set, train the weights and biases of the network using
         gradient descent. Back Propagation is used to calculate the derivatives
@@ -133,21 +120,22 @@ class NeuralNetwork:
          ...
          (inputs, outputs)]
 
-         The inputs and outputs are lists
+        The inputs and outputs are lists
         """
 
         n = len(training_set)
 
         for it in range(its):
-            if it % 10 == 0:
+            if verbose and it % 10 == 0:
                 print("Iterations:", it)
 
+            # Will have the sums of gradients for all examples
             sum_cost_w = [np.zeros((iv, self.layers[i-1]))
                           for i, iv in enumerate(self.layers)]
             sum_cost_b = [np.zeros((iv, 1))
                           for i, iv in enumerate(self.layers)]
 
-            # Get the weights and bias
+            # Run every training example and sum its gradient
             for i in training_set:
                 self.backPropagation(i[0], i[1])
 
@@ -157,8 +145,9 @@ class NeuralNetwork:
                 for l, vl in enumerate(self.cost_b[1:], 1):
                     sum_cost_b[l] += self.cost_b[l]
 
-            for i, iv in enumerate(sum_cost_w[1:], 1):
-                self.weights[i] -= step/n * iv
+            # Update weights and biases using the rules
+            for l, lv in enumerate(sum_cost_w[1:], 1):
+                self.weights[l] -= step/n * lv
 
-            for i, iv in enumerate(sum_cost_b):
-                self.bias[i] -= step/n * iv
+            for l, lv in enumerate(sum_cost_b):
+                self.bias[l] -= step/n * lv
