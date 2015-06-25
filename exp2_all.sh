487dc13798746181916d41b9a8f0ea3474576897
petermlm <pedromelgueira@gmail.com>
2015-06-25 17:18:12 +0100

    Removed funny comment

--- exp2_all.sh
+++ exp2_all.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env sh
 
-# I know it could be better. I just don't really care right now
 python3 -m exp2.exp2 its_var
 python3 -m exp2.exp2 its_var 1
 python3 -m exp2.exp2 its_var 2
 python3 -m exp2.exp2 its_var 3
+
 python3 -m exp2.exp2 step_var
 python3 -m exp2.exp2 step_var 1
 python3 -m exp2.exp2 step_var 2
 python3 -m exp2.exp2 step_var 3
+
 python3 -m exp2.exp2 train_var
 python3 -m exp2.exp2 train_var 1
 python3 -m exp2.exp2 train_var 2
