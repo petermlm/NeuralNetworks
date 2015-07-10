b1f879632ebe9b080a941dd82865679be59d1dc3
petermlm <pedromelgueira@gmail.com>
2015-07-10 13:15:43 +0100

    Corrected thing in experiments

--- exp2_all.sh
+++ exp2_all.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env sh
 
 python3 -m exp2.exp2 its_var
-python3 -m exp2.exp2 its_var 1
 python3 -m exp2.exp2 its_var 2
 python3 -m exp2.exp2 its_var 3
+python3 -m exp2.exp2 its_var 4
 
 python3 -m exp2.exp2 step_var
-python3 -m exp2.exp2 step_var 1
 python3 -m exp2.exp2 step_var 2
 python3 -m exp2.exp2 step_var 3
+python3 -m exp2.exp2 step_var 4
 
 python3 -m exp2.exp2 train_var
-python3 -m exp2.exp2 train_var 1
 python3 -m exp2.exp2 train_var 2
 python3 -m exp2.exp2 train_var 3
+python3 -m exp2.exp2 train_var 4
--- exp2_ce_all.sh
+++ exp2_ce_all.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env sh
 
 python3 -m exp2_ce.exp2_ce its_var
-python3 -m exp2_ce.exp2_ce its_var 1
 python3 -m exp2_ce.exp2_ce its_var 2
 python3 -m exp2_ce.exp2_ce its_var 3
+python3 -m exp2_ce.exp2_ce its_var 4
 
 python3 -m exp2_ce.exp2_ce step_var
-python3 -m exp2_ce.exp2_ce step_var 1
 python3 -m exp2_ce.exp2_ce step_var 2
 python3 -m exp2_ce.exp2_ce step_var 3
+python3 -m exp2_ce.exp2_ce step_var 4
 
 python3 -m exp2_ce.exp2_ce train_var
-python3 -m exp2_ce.exp2_ce train_var 1
 python3 -m exp2_ce.exp2_ce train_var 2
 python3 -m exp2_ce.exp2_ce train_var 3
+python3 -m exp2_ce.exp2_ce train_var 4
